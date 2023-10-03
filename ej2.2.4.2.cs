7cf7be4de56174c36fb4e5589165ef2ab3ee4f0c
Alexpo02 <alexexposito2@gmail.com>
2023-10-03 16:13:26 +0200

    Añadiendo ejercicio c#

---
+++ ej2.2.4.2.cs
@@ -0,0 +1,14 @@
+/*(2.2.4.2) Crea un programa que contenga un bucle sin fin que muestre los
+números enteros positivos a partir del uno, separados por un espacio en blanco.*/
+using System;
+
+class ej2_2_4_2
+{
+	static void Main()
+	{
+		for (int i = 1; i > 0; i++)
+		{
+			Console.Write("{0} ", i);
+		}
+	}
+}
